1c2060ae49fbb72b3fbd95dc3406f759a7285d54
supagetto <5up4g3tt0@gmail.com>
2026-03-22 18:04:30 +0800

    🐛 Fix deprecated nvim-treesitter-textobjects config

--- neovim/.config/nvim/lua/plugins/treesitter/textobjects.lua
+++ neovim/.config/nvim/lua/plugins/treesitter/textobjects.lua
@@ -14,52 +14,99 @@ M.dependencies = {
 
 M.event = 'VeryLazy'
 
+M.init = function()
+  require('utils').set_global('no_plugin_maps', true)
+end
+
 M.opts = function()
   return {
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-          ['aB'] = '@block.outer',
-          ['iB'] = '@block.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          [']f'] = '@function.outer',
-          [']c'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']F'] = '@function.outer',
-          [']C'] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-          ['[c'] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[F'] = '@function.outer',
-          ['[C'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<Leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<Leader>A'] = '@parameter.inner',
-        },
-      },
+
+    select = {
+      lookahead = true,
+    },
+    move = {
+      set_jumps = true,
     },
   }
+end
+
+M.config = function(_, opts)
+  require('nvim-treesitter-textobjects').setup(opts)
+
+  require('utils').set_keymaps(
+    {
+      { 'x', 'o' },
+      'af',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects') end,
+    },
+    {
+      { 'x', 'o' },
+      'if',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects') end,
+    },
+    {
+      { 'x', 'o' },
+      'ac',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@class.outer', 'textobjects') end,
+    },
+    {
+      { 'x', 'o' },
+      'ic',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@class.inner', 'textobjects') end,
+    },
+    {
+      { 'x', 'o' },
+      'aB',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@block.outer', 'textobjects') end,
+    },
+    {
+      { 'x', 'o' },
+      'iB',
+      function() require('nvim-treesitter-textobjects.select').select_textobject('@block.inner', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      ']f',
+      function() require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      ']c',
+      function() require('nvim-treesitter-textobjects.move').goto_next_start('@class.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      ']F',
+      function() require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      ']C',
+      function() require('nvim-treesitter-textobjects.move').goto_next_end('@class.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      '[f',
+      function() require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      '[c',
+      function() require('nvim-treesitter-textobjects.move').goto_previous_start('@class.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      '[F',
+      function() require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects') end,
+    },
+    {
+      { 'n', 'x', 'o' },
+      '[C',
+      function() require('nvim-treesitter-textobjects.move').goto_previous_end('@class.outer', 'textobjects') end,
+    },
+    { 'n', '<Leader>a', function() require('nvim-treesitter-textobjects.swap').swap_next('@parameter.inner') end },
+    { 'n', '<Leader>A', function() require('nvim-treesitter-textobjects.swap').swap_previous('@parameter.inner') end }
+  )
 end
 
 return M
